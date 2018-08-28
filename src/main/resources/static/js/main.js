604485ce95ce2efc8dd00cbf88f6e3aedd3017d7
aeliwat <aeliwat@atypon.com>
2018-08-28 16:18:52 +0300

    Fixing XMLHTTP  Instance for IE6, IE5

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,62 +1,69 @@
-
 $(document).on("ready", init);
 
-function init(){
-    $(document).on('change','#select_citation_style',sendRequest);
+function init() {
+    $(document).on('change', '#select_citation_style', sendRequest);
 }
 
-function sendRequest(){
-    var selectCiteStyle=$('#select_citation_style').val();
+function sendRequest() {
+    var selectCiteStyle = $('#select_citation_style').val();
 
-    if(selectCiteStyle){
+    if (selectCiteStyle) {
 
-    // add spinner
+        // add spinner
         $('.cls-response').html("<span>Loading ... </span>");
+        console.log("-- Start Processing ---- ");
+        // Get the citations that we are supposed to render, in the CSL-json format
+        var xhr = getXMLHTTPInstance();
+        xhr.open('GET', '/cite/getMetaData', false);
+        xhr.send(null);
+        var citations = JSON.parse(xhr.responseText);
+        console.log("-- Finsh Get MetaData ---- ");
+// Initialize a system object, which contains two methods needed by the engine
 
-    // Get the citations that we are supposed to render, in the CSL-json format
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', '/cite/getMetaData', false);
-    xhr.send(null);
-    var citations = JSON.parse(xhr.responseText);
-
-// Initialize a system object, which contains two methods needed by the
-// engine.
     citeprocSys = {
-        // Given a language tag in RFC-4646 form, this method retrieves the
-        // locale definition file.
-        retrieveLocale: function (lang){
-            var xhr = new XMLHttpRequest();
-            xhr.open('GET', 'https://raw.githubusercontent.com/Juris-M/citeproc-js-docs/master/locales-en-US.xml', false);
-            xhr.send(null);
-            return xhr.responseText;
-        },
+            // Given a language tag in RFC-4646 form, this method retrieves the
+            // locale definition file.
+            retrieveLocale: function (lang) {
+                var xhr = getXMLHTTPInstance();
+                xhr.open('GET', 'https://raw.githubusercontent.com/Juris-M/citeproc-js-docs/master/locales-en-US.xml', false);
+                xhr.send(null);
+                return xhr.responseText;
+            },
 
-        // Given an identifier, this retrieves one citation item.  This method
-        // must return a valid CSL-JSON object.
-        retrieveItem: function(id){
-            return citations[id];
+            // Given an identifier, this retrieves one citation item.  This method
+            // must return a valid CSL-JSON object.
+            retrieveItem: function (id) {
+                return citations[id];
+            }
+        };
+        console.log("-- Finsh Read Locale definition file. ---- ");
+
+        var citeproc = getProcessor(selectCiteStyle);
+        console.log("-- Finsh Intialize Processer. ---- ");
+        var itemIDs = [];
+        for (var key in citations) {
+            itemIDs.push(key);
         }
-    };
-
-    var citeproc = getProcessor(selectCiteStyle);
-
-    var itemIDs = [];
-    for (var key in citations) {
-        itemIDs.push(key);
-    }
-    citeproc.updateItems(itemIDs);
-    var bibResult = citeproc.makeBibliography();
-    $('.cls-response').html(bibResult[1]);
+        citeproc.updateItems(itemIDs);
+        var bibResult = citeproc.makeBibliography();
+        $('.cls-response').html(bibResult[1]);
     }
 
 }
 
+function getXMLHTTPInstance() {
+    return window.XMLHttpRequest ?
+        // IE7+, Firefox, Chrome, Opera, Safari
+        new XMLHttpRequest() :
+        //  IE6, IE5
+        new ActiveXObject("Microsoft.XMLHTTP");
+}
 // Given the identifier of a CSL style, this function instantiates a CSL.Engine
 // object that can render citations in that style.
 function getProcessor(styleID) {
     // Get the CSL style as a serialized string of XML
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', '/cite/readCSLFile/'+styleID, false);
+    var xhr = getXMLHTTPInstance();
+    xhr.open('GET', '/cite/readCSLFile/' + styleID, false);
     xhr.send(null);
     var styleAsText = xhr.responseText;
 
